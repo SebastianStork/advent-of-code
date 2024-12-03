f2e3a50169064a09ce97abab8618a363e208f4c0
SebastianStork <sebastian.stork@pm.me>
2024-12-03 20:41:39 +0100

    Add nix devshell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.qtcreator =
+        pkgs.mkShell.override
+          {
+            stdenv = pkgs.clangStdenv;
+          }
+          {
+            packages = with pkgs; [
+              gdb
+              clang-tools
+              qt6.full
+              qtcreator
+            ];
+          };
+
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+    };
+}
