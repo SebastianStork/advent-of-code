87a50f0befbeca4356ddfea444208921bc1e68b6
SebastianStork <sebastian.stork@pm.me>
2024-12-06 13:30:44 +0100

    Solve 2024 day 2 part 2

--- 2024/day-02/main.cpp
+++ 2024/day-02/main.cpp
@@ -17,22 +17,50 @@ void readInput(vector<vector<int>> &list)
     }
 }
 
-int calculateNumberOfSafeReports(const vector<vector<int>> &list)
+int getIndexOfUnsafeValue(const vector<int> &report)
+{
+    const int MAX_SAFE_DIFFERENCE = 3;
+
+    int directionOfChange = (report.front() < report.back()) ? 1 : -1;
+    for (size_t i = 0; i < report.size() - 1; i++) {
+        int levelDifference = (report[i + 1] - report[i]) * directionOfChange;
+        if (levelDifference <= 0 || levelDifference > MAX_SAFE_DIFFERENCE) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+bool isSafeReport(const vector<int> &report, const bool applyDampener)
+{
+    int indexOfUnsafeValue = getIndexOfUnsafeValue(report);
+
+    if (indexOfUnsafeValue == -1) {
+        return true;
+    }
+
+    if (!applyDampener) {
+        return false;
+    }
+
+    vector<int> tempReport = report;
+    int unsafeValue = report[indexOfUnsafeValue];
+    tempReport.erase(tempReport.begin() + indexOfUnsafeValue);
+    if (getIndexOfUnsafeValue(tempReport) == -1) {
+        return true;
+    }
+
+    tempReport.insert(tempReport.begin() + indexOfUnsafeValue, unsafeValue);
+    tempReport.erase(tempReport.begin() + indexOfUnsafeValue + 1);
+    return getIndexOfUnsafeValue(tempReport) == -1;
+}
+
+int calculateNumberOfSafeReports(const vector<vector<int>> &list, const bool applyDampener)
 {
     int numberOfSafeReports = 0;
-    for (vector<int> report : list) {
-        bool safe = true;
-
-        int directionOfChange = (report[0] < report[1]) ? 1 : -1;
-        for (size_t i = 0; i < report.size() - 1; i++) {
-            int levelDifference = (report[i + 1] - report[i]) * directionOfChange;
-
-            if (levelDifference <= 0 || levelDifference > 3) {
-                safe = false;
-                break;
-            }
-        }
-        numberOfSafeReports += safe;
+    for (const vector<int> &report : list) {
+        numberOfSafeReports += isSafeReport(report, applyDampener);
     }
     return numberOfSafeReports;
 }
@@ -43,5 +71,9 @@ int main()
     readInput(list);
 
     // Part one
-    cout << "Number of safe reports: " << calculateNumberOfSafeReports(list) << endl;
+    cout << "Number of safe reports: " << calculateNumberOfSafeReports(list, false) << endl;
+
+    // Part two
+    cout << "Number of safe reports (when taking the dampener into account): "
+         << calculateNumberOfSafeReports(list, true) << endl;
 }
