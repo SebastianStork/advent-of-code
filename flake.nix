cf1df57e6f6cfefaa578822b413d46cc4598ad14
SebastianStork <sebastian.stork@pm.me>
2025-10-04 20:04:50 +0200

    Add rust devshell

--- flake.nix
+++ flake.nix
@@ -1,29 +1,45 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    { self, nixpkgs }:
+    { nixpkgs, rust-overlay, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       devShells.${system} = {
-        cpp =
-          pkgs.mkShell.override
-            {
-              stdenv = pkgs.clangStdenv;
-            }
-            {
-              packages = with pkgs; [
-                gdb
-                clang-tools
-                qt6.full
-              ];
-            };
-        go = pkgs.mkShell {
+        cpp = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+          packages = with pkgs; [
+            gdb
+            clang-tools
+            qt6.full
+          ];
+        };
+
+        go = pkgs.mkShellNoCC {
           packages = with pkgs; [ go ];
         };
+
+        rust =
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ rust-overlay.overlays.default ];
+            };
+            rust-toolchain = pkgs.rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; };
+          in
+          pkgs.mkShellNoCC {
+            packages = [ rust-toolchain ];
+            RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
+          };
       };
 
       formatter.${system} = pkgs.nixfmt-tree;
